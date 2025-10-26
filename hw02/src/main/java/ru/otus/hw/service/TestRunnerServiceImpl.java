873167fce9b2809bafa3954cee9eb8bbf49a2f78
drugpamir <toll_aa@mail.ru>
2025-10-26 22:45:11 +0300

    Обработка исключений на верхнем уровне

--- hw02/src/main/java/ru/otus/hw/service/TestRunnerServiceImpl.java
+++ hw02/src/main/java/ru/otus/hw/service/TestRunnerServiceImpl.java
@@ -2,6 +2,7 @@ package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.otus.hw.exceptions.QuestionReadException;
 
 @RequiredArgsConstructor
 @Service
@@ -13,10 +14,18 @@ public class TestRunnerServiceImpl implements TestRunnerService {
 
     private final ResultService resultService;
 
+    private final IOService ioService;
+
     @Override
     public void run() {
         var student = studentService.determineCurrentStudent();
-        var testResult = testService.executeTestFor(student);
-        resultService.showResult(testResult);
+        try {
+            var testResult = testService.executeTestFor(student);
+            resultService.showResult(testResult);
+        }  catch (QuestionReadException e) {
+            ioService.printLine("Question loading failed!");
+        } catch (Exception e) {
+            ioService.printLine("Unknown error occurred!");
+        }
     }
 }
