aae81abdf4c11205238dc950ff7148806ba59958
drugpamir <toll_aa@mail.ru>
2025-10-26 22:40:08 +0300

    Законченная реализация программы тестирования студентов

--- hw02/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw02/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -2,9 +2,14 @@ package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.otus.hw.config.AppProperties;
 import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Question;
 import ru.otus.hw.domain.Student;
 import ru.otus.hw.domain.TestResult;
+
+import java.util.List;
+import java.util.Random;
 
 @RequiredArgsConstructor
 @Service
@@ -14,18 +19,40 @@ public class TestServiceImpl implements TestService {
 
     private final QuestionDao questionDao;
 
+    private final AppProperties appProperties;
+
     @Override
     public TestResult executeTestFor(Student student) {
         ioService.printLine("");
         ioService.printFormattedLine("Please answer the questions below%n");
+
         var questions = questionDao.findAll();
+        var testQuestions = getRandomQuestions(questions, appProperties.getRightAnswersCountToPass());
+
         var testResult = new TestResult(student);
 
-        for (var question: questions) {
-            var isAnswerValid = false; // Задать вопрос, получить ответ
+        for (var question : testQuestions) {
+            ioService.printLine(question.text());
+
+            var answersBuilder = new StringBuilder();
+            for (int i = 0; i < question.answers().size(); i++) {
+                answersBuilder.append(String.format("%d: %s\n", i + 1, question.answers().get(i).text()));
+            }
+
+            int answer = ioService.readIntForRangeWithPrompt(1, question.answers().size(), answersBuilder.toString(), "Answer input error");
+
+            var isAnswerValid = question.answers().get(answer - 1).isCorrect();
             testResult.applyAnswer(question, isAnswerValid);
         }
         return testResult;
+    }
+
+    private List<Question> getRandomQuestions(List<Question> questions, int count) {
+        return new Random().ints(0, questions.size()).boxed()
+                .distinct()
+                .limit(count)
+                .map(questions::get)
+                .toList();
     }
 }
 
