5cc1392cbcb2d17ff49e2188cb95bb11f9e8ed30
drugpamir <toll_aa@mail.ru>
2025-10-27 22:55:27 +0300

    Настройки @Configuration и @PropertySource вынесены в отдельный класс. Тесты CommonHwTest проходят.

--- hw02/src/main/java/ru/otus/hw/config/AppProperties.java
+++ hw02/src/main/java/ru/otus/hw/config/AppProperties.java
@@ -2,17 +2,21 @@ package ru.otus.hw.config;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
-    import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
 
 @Data
-@Configuration
-@PropertySource("classpath:application.properties")
+@Component
 public class AppProperties implements TestConfig, TestFileNameProvider {
 
-    @Value("${test.rightAnswersCountToPass}")
     private int rightAnswersCountToPass;
 
-    @Value("${test.fileName}")
     private String testFileName;
+
+    public AppProperties(
+            @Value("${test.rightAnswersCountToPass}") int rightAnswersCountToPass,
+            @Value("${test.fileName}") String testFileName
+    ) {
+        this.rightAnswersCountToPass = rightAnswersCountToPass;
+        this.testFileName = testFileName;
+    }
 }
