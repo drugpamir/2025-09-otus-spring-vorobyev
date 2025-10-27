77e0019b4eb59960d8b8dc0d3dc0df0685d402b0
drugpamir <toll_aa@mail.ru>
2025-10-28 00:01:38 +0300

    Интеграционный тест CsvQuestionDao на точное соответствие считанных и записанных в файл данных

--- hw02/src/test/java/ru/otus/hw/dao/CsvQuestionDaoTest.java
+++ hw02/src/test/java/ru/otus/hw/dao/CsvQuestionDaoTest.java
@@ -3,6 +3,7 @@ package ru.otus.hw.dao;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.domain.Answer;
 import ru.otus.hw.domain.Question;
 import ru.otus.hw.exceptions.QuestionReadException;
 
@@ -39,9 +40,22 @@ class CsvQuestionDaoTest {
     }
 
     @Test
-    void shouldReturnListOfQuestions() {
+    void shouldReturnExpectedCountOfQuestions() {
         List<Question> questions = csvQuestionDao.findAll();
         assertThat(questions.size()).isEqualTo(5);
+    }
+
+    @Test
+    void shouldReturnCorrectQuestionsDate() {
+        List<Question> expected = List.of(
+                new Question("Question 1", List.of(new Answer("Answer 1", false), new Answer("Answer 2", true))),
+                new Question("Question 2", List.of(new Answer("Answer 1", true), new Answer("Answer 2", false))),
+                new Question("Question 3", List.of(new Answer("Answer 1", false), new Answer("Answer 2", true), new Answer("Answer 3", false))),
+                new Question("Question 4", List.of(new Answer("Answer 1", false), new Answer("Answer 2", false), new Answer("Answer 3", true))),
+                new Question("Question 5", List.of(new Answer("Answer 1", false), new Answer("Answer 2", true), new Answer("Answer 3", true)))
+        );
+        List<Question> actual = csvQuestionDao.findAll();
+        assertThat(actual).isEqualTo(expected);
     }
 
     @Test
