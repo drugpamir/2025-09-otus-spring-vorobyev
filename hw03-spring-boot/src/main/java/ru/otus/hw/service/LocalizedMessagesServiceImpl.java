2126046463b8d0ace58c6fe583404d8baadee0cb
drugpamir <toll_aa@mail.ru>
2025-10-29 01:01:15 +0300

    Чтение шаблонов сообщений в LocalizedMessagesServiceImpl

--- hw03-spring-boot/src/main/java/ru/otus/hw/service/LocalizedMessagesServiceImpl.java
+++ hw03-spring-boot/src/main/java/ru/otus/hw/service/LocalizedMessagesServiceImpl.java
@@ -1,6 +1,7 @@
 package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 import ru.otus.hw.config.LocaleConfig;
 
@@ -10,9 +11,10 @@ public class LocalizedMessagesServiceImpl implements LocalizedMessagesService {
 
     private final LocaleConfig localeConfig;
 
-    // Доделать
+    private final MessageSource messageSource;
+
     @Override
     public String getMessage(String code, Object... args) {
-        return null;
+        return messageSource.getMessage(code, args, localeConfig.getLocale());
     }
 }
