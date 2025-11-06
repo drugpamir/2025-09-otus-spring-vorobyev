2f5f16266eadb1f2261d794f687ac1717d0fc8dc
drugpamir <toll_aa@mail.ru>
2025-11-07 01:10:01 +0300

    Тесты переписаны с MokitoExtension на @SpringBootTest, Shell-оболочка отключается с помощью профиля test и соответствующего конфигурационного файла

--- hw04-spring-shell/src/test/java/ru/otus/hw/service/TestServiceImplTest.java
+++ hw04-spring-shell/src/test/java/ru/otus/hw/service/TestServiceImplTest.java
@@ -2,10 +2,10 @@ package ru.otus.hw.service;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import ru.otus.hw.config.AppProperties;
 import ru.otus.hw.dao.QuestionDao;
 import ru.otus.hw.domain.Answer;
@@ -17,19 +17,20 @@ import java.util.List;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@ActiveProfiles("test")
 class TestServiceImplTest {
 
-    @Mock
+    @MockitoBean
     private LocalizedIOService ioService;
 
-    @Mock
+    @MockitoBean
     private QuestionDao questionDao;
 
-    @Mock
+    @MockitoBean
     private AppProperties appProperties;
 
-    @InjectMocks
+    @Autowired
     private TestServiceImpl testService;
 
     @BeforeEach
