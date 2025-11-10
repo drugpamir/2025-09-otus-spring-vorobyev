98d381aad3b47862b9e05565d2316bd1f64d465e
drugpamir <toll_aa@mail.ru>
2025-11-11 01:19:26 +0300

    Реализация findAll-запроса JdbcBookRepository (включая подгрузку и привязку к каждой книге её автора и жанров)

--- hw05-jdbc-hard/src/main/java/ru/otus/hw/repositories/JdbcBookRepository.java
+++ hw05-jdbc-hard/src/main/java/ru/otus/hw/repositories/JdbcBookRepository.java
@@ -4,20 +4,24 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
+import ru.otus.hw.models.Author;
 import ru.otus.hw.models.Book;
 import ru.otus.hw.models.Genre;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
 public class JdbcBookRepository implements BookRepository {
+
+    private final NamedParameterJdbcTemplate jdbc;
 
     private final GenreRepository genreRepository;
 
@@ -49,16 +53,29 @@ public class JdbcBookRepository implements BookRepository {
     }
 
     private List<Book> getAllBooksWithoutGenres() {
-        return new ArrayList<>();
+        var sql = """
+                select b.id, b.title, a.id as a_id, a.full_name as a_full_name\s
+                from books b\s
+                    left join authors a\s
+                        on a.id = b.author_id
+               \s""";
+        return jdbc.getJdbcOperations().query(sql, new BookRowMapper());
     }
 
     private List<BookGenreRelation> getAllGenreRelations() {
-        return new ArrayList<>();
+        var sql = "select book_id, genre_id from books_genres";
+        return jdbc.getJdbcOperations().query(sql, new BookGenreRelationMapper());
     }
 
     private void mergeBooksInfo(List<Book> booksWithoutGenres, List<Genre> genres,
                                 List<BookGenreRelation> relations) {
-        // Добавить книгам (booksWithoutGenres) жанры (genres) в соответствии со связями (relations)
+        relations.forEach(relation -> {
+            Optional<Book> book = booksWithoutGenres.stream().filter(b -> b.getId() == relation.bookId).findFirst();
+            Optional<Genre> genre = genres.stream().filter(g -> g.getId() == relation.genreId).findFirst();
+            if (book.isPresent() && genre.isPresent()) {
+                book.get().getGenres().add(genre.get());
+            }
+        });
     }
 
     private Book insert(Book book) {
@@ -94,7 +111,20 @@ public class JdbcBookRepository implements BookRepository {
 
         @Override
         public Book mapRow(ResultSet rs, int rowNum) throws SQLException {
-            return null;
+            var bookId = rs.getLong("id");
+            var bookTitle = rs.getString("title");
+            var author = new Author(rs.getLong("a_id"), rs.getString("a_full_name"));
+            return new Book(bookId, bookTitle, author, new LinkedList<>());
+        }
+    }
+
+    private static class BookGenreRelationMapper implements RowMapper<BookGenreRelation> {
+
+        @Override
+        public BookGenreRelation mapRow(ResultSet rs, int rowNum) throws SQLException {
+            var bookId = rs.getLong("book_id");
+            var genreId = rs.getLong("genre_id");
+            return new BookGenreRelation(bookId, genreId);
         }
     }
 
