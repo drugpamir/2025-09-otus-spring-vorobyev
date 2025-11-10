ec317d914dc6b2490ae3282ae00205ea3545d8ef
drugpamir <toll_aa@mail.ru>
2025-11-11 00:16:29 +0300

    Реализация select-запросов JdbcGenreRepository (с использованием NamedParameterJdbcOperations)

--- hw05-jdbc-hard/src/main/java/ru/otus/hw/repositories/JdbcGenreRepository.java
+++ hw05-jdbc-hard/src/main/java/ru/otus/hw/repositories/JdbcGenreRepository.java
@@ -1,33 +1,32 @@
 package ru.otus.hw.repositories;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.stereotype.Repository;
 import ru.otus.hw.models.Genre;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Repository
 public class JdbcGenreRepository implements GenreRepository {
 
-    private final JdbcOperations jdbc;
+    private final NamedParameterJdbcOperations jdbc;
 
     @Override
     public List<Genre> findAll() {
-        return jdbc.query("select id, name from genres", new GnreRowMapper());
+        return jdbc.getJdbcOperations().query("select id, name from genres", new GnreRowMapper());
     }
 
     @Override
     public List<Genre> findAllByIds(Set<Long> ids) {
-        var query = String.format("select id, name from genres where id in (%s)",
-                ids.stream().map(String::valueOf).collect(Collectors.joining(",")));
-        return jdbc.query(query, new GnreRowMapper());
+        var params = Map.of("ids", ids);
+        return jdbc.query("select id, name from genres where id in (:ids)", params, new GnreRowMapper());
     }
 
     private static class GnreRowMapper implements RowMapper<Genre> {
