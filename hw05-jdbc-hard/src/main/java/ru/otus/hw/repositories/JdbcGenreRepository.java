e0867202f8f7dd3329856c423bcb604c3bd7ed27
drugpamir <toll_aa@mail.ru>
2025-11-10 23:59:54 +0300

    Реализация select-запросов JdbcGenreRepository (с использованием JdbcOperations)

--- hw05-jdbc-hard/src/main/java/ru/otus/hw/repositories/JdbcGenreRepository.java
+++ hw05-jdbc-hard/src/main/java/ru/otus/hw/repositories/JdbcGenreRepository.java
@@ -1,33 +1,40 @@
 package ru.otus.hw.repositories;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import ru.otus.hw.models.Genre;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
+@RequiredArgsConstructor
 @Repository
 public class JdbcGenreRepository implements GenreRepository {
 
+    private final JdbcOperations jdbc;
+
     @Override
     public List<Genre> findAll() {
-        return new ArrayList<>();
+        return jdbc.query("select id, name from genres", new GnreRowMapper());
     }
 
     @Override
     public List<Genre> findAllByIds(Set<Long> ids) {
-        return new ArrayList<>();
+        var query = String.format("select id, name from genres where id in (%s)",
+                ids.stream().map(String::valueOf).collect(Collectors.joining(",")));
+        return jdbc.query(query, new GnreRowMapper());
     }
 
     private static class GnreRowMapper implements RowMapper<Genre> {
 
         @Override
         public Genre mapRow(ResultSet rs, int i) throws SQLException {
-            return null;
+            return new Genre(rs.getLong("id"), rs.getString("name"));
         }
     }
 }
